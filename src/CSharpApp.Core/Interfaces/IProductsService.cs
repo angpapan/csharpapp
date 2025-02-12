c122771736a851d23d93034857d857940518c4a7
Angelos Papanikolaou <sdi1800153@di.uoa.gr>
2025-02-12 19:08:36 +0200

    filled IProductService with the needed methods for getOne and create

--- src/CSharpApp.Core/Interfaces/IProductsService.cs
+++ src/CSharpApp.Core/Interfaces/IProductsService.cs
@@ -1,6 +1,10 @@
+using CSharpApp.Core.Dtos.Products;
+
 namespace CSharpApp.Core.Interfaces;
 
 public interface IProductsService
 {
-    Task<IReadOnlyCollection<Product>> GetProducts();
+    Task<IReadOnlyCollection<Product>> GetProducts(CancellationToken cancellationToken = default);
+    Task<Product> GetProductById(int id, CancellationToken cancellationToken = default);
+    Task<Product> CreateProduct(CreateProduct product, CancellationToken cancellationToken = default);
 }
