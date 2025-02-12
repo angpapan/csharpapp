a6ef2ed7f8a656ae603d01b5c40625dc3f64e6bf
Angelos Papanikolaou <sdi1800153@di.uoa.gr>
2025-02-12 23:12:39 +0200

    add extension method to simplify convertion to strongly typed objects

---
+++ src/CSharpApp.Application/Extensions/HttpResponseMessageExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpApp.Application.Extensions
+{
+    internal static class HttpResponseMessageExtensions
+    {
+        internal static async Task<T> ConvertToOrThrow<T>(this HttpResponseMessage response, CancellationToken cancellationToken = default)
+        {
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync(cancellationToken);
+            return JsonSerializer.Deserialize<T>(content);
+        }
+    }
+}
