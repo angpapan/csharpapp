aae4e780eb13a36c45ec1d9e2941b3325ec5e96b
Angelos Papanikolaou <sdi1800153@di.uoa.gr>
2025-02-12 22:58:38 +0200

    add category dtos and ICategoryService

---
+++ src/CSharpApp.Core/Dtos/Categories/CreateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpApp.Core.Dtos.Categories;
+
+public class CreateCategory
+{
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("image")]
+    public string? Image { get; set; }
+}
---
+++ src/CSharpApp.Core/Interfaces/ICategoriesService.cs
@@ -0,0 +1,11 @@
+﻿using CSharpApp.Core.Dtos.Categories;
+
+namespace CSharpApp.Core.Interfaces;
+
+public interface ICategoriesService
+{
+    public Task<IReadOnlyCollection<Category>> GetCategories(CancellationToken cancellationToken = default);
+    public Task<Category> GetCategoryById(int id, CancellationToken cancellationToken = default);
+    public Task<Category> CreateCategory(CreateCategory category, CancellationToken cancellationToken = default);
+    public Task<Category> UpdateCategory(UpdateCategory category, CancellationToken cancellationToken = default);
+}
