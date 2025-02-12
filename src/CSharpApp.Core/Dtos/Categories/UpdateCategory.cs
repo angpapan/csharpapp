5c9cbbadaeefa160f9b1801e033e222447be4fe9
Angelos Papanikolaou <sdi1800153@di.uoa.gr>
2025-02-12 23:13:35 +0200

    fix UpdateCategoryDto

--- src/CSharpApp.Core/Dtos/Categories/UpdateCategory.cs
+++ src/CSharpApp.Core/Dtos/Categories/UpdateCategory.cs
@@ -6,8 +6,8 @@ public class UpdateCategory
     public int Id { get; set; }
 
     [JsonPropertyName("name")]
-    public string? Name { get; set; }
+    public string? Name { get; set; } = null;
 
     [JsonPropertyName("image")]
-    public string? Image { get; set; }
+    public string? Image { get; set; } = null;
 }
