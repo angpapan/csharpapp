b5191d4f2ee3d034da0eccfd1269dda8b23ee479
Angelos Papanikolaou <sdi1800153@di.uoa.gr>
2025-02-13 10:38:14 +0200

    declare and implement AuthService

---
+++ src/CSharpApp.Application/Auth/AuthService.cs
@@ -0,0 +1,58 @@
+﻿using CSharpApp.Application.Extensions;
+using CSharpApp.Core.Dtos.Auth;
+using CSharpApp.Core.Settings.HttpClient;
+using CSharpApp.Core.Settings.RestApi;
+using Microsoft.Extensions.Caching.Memory;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace CSharpApp.Application.Auth;
+
+public class AuthService : IAuthService
+{
+    private readonly HttpClient _httpClient;
+    private readonly RestApiSettings _restApiSettings;
+    private readonly IMemoryCache _cache;
+
+    public AuthService(
+        IOptions<RestApiSettings> restApiSettings,
+        IOptions<HttpClientSettings> httpClientSettings,
+        IHttpClientFactory httpFactory,
+        IMemoryCache cache
+        )
+    {
+        _httpClient = httpFactory.CreateClient(httpClientSettings.Value.Name);
+        _restApiSettings = restApiSettings.Value;
+        _cache = cache;
+    }
+
+    public async Task<LoginResponse> Login(LoginRequest request, CancellationToken cancellationToken = default)
+    {
+        JsonContent bodyContent = JsonContent.Create(request);
+
+        var response = await _httpClient.PostAsync(_restApiSettings.Auth, bodyContent, cancellationToken);
+        var res = await response.ConvertToOrThrow<LoginResponse>(cancellationToken);
+
+        // add to cache
+        string token = res.AccessToken;
+        var expiration = DateTimeOffset.UtcNow.AddMinutes((double)_restApiSettings.AuthTokenExpirationInMinutes);
+        _cache.Set(_restApiSettings.AuthTokenCacheId, token, expiration);
+
+        return res;
+    }
+
+    public async Task<Profile> GetProfile(CancellationToken cancellationToken = default)
+    {
+        if (!_cache.TryGetValue(_restApiSettings.AuthTokenCacheId, out string token))
+        {
+            token = string.Empty;
+        }
+
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        var response = await _httpClient.GetAsync($"{_restApiSettings.Auth}/profile", cancellationToken);
+        var res = await response.ConvertToOrThrow<Profile>(cancellationToken);
+
+        return res;
+    }
+
+}
---
+++ src/CSharpApp.Core/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using CSharpApp.Core.Dtos.Auth;
+
+namespace CSharpApp.Core.Interfaces;
+
+public interface IAuthService
+{
+    Task<LoginResponse> Login(LoginRequest request, CancellationToken cancellationToken = default);
+    Task<Profile> GetProfile(CancellationToken cancellationToken = default);
+}
