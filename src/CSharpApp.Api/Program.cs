dafc536e5431648552642d8541d46eeb3272b79b
Angelos Papanikolaou <sdi1800153@di.uoa.gr>
2025-02-13 09:41:08 +0200

    create and register exception handling middleware

--- src/CSharpApp.Api/Program.cs
+++ src/CSharpApp.Api/Program.cs
@@ -1,4 +1,5 @@
 using CSharpApp.Api.Endpoints;
+using CSharpApp.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +21,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors();
 //app.UseHttpsRedirection();
 
